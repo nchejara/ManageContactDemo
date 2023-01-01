7b62d54441c202820a708761adfb0732dbd851ef
NarendraKumar-Chejara <narendrakumar.chejara@microfocus.com>
2023-01-01 21:22:33 +0800

    add -y flag in database.sh

--- setup_env/database.sh
+++ setup_env/database.sh
@@ -4,4 +4,4 @@ sudo apt install wget ca-certificates
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 sudo apt update
-apt install postgresql postgresql-contrib
+apt install -y postgresql postgresql-contrib
